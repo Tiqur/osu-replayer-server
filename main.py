1329bff8449219f924e13e604ef32e181c3ae6b9
Tiqur <Tiqur7@gmail.com>
2025-04-07 20:49:11 -0700

    Change default port

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='OSU Replay Server')
     parser.add_argument('--host', default='0.0.0.0', help='Host to bind to')
-    parser.add_argument('--port', type=int, default=8000, help='Port to bind to')
+    parser.add_argument('--port', type=int, default=8727, help='Port to bind to')
     parser.add_argument('--debug', action='store_true', help='Enable debug mode')
     
     args = parser.parse_args()
